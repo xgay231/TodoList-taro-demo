ab2d83ac6f3e15caca10fae32f6e261ae406b83f
xgay231 <xgay231@qq.com>
2025-05-05 09:52:37 +0800

    feat: 更新项目配置，添加待办事项页面及功能

---
+++ src/pages/demo/demo.tsx
@@ -0,0 +1,31 @@
+import { Button, Input, View } from '@tarojs/components'
+import { useRef } from 'react'
+
+// 1.需要有一个输入框 用来输入我们todoDesc
+// 2.有一个确认按钮 点击按钮可以添加到todoList中
+// 3.有一个列表 用来展示我们添加的todoList
+// 4.列表item需要有一个删除按钮 点击可以删除当前的todoitem
+export default ()=> {
+  const[todoList, setTodoList] = useState([])
+  const InputRef = useRef(null)
+
+  const onsubmit = () => {
+    console.log(InputRef.current.value)
+    setTodoList([
+      ...todoList,
+      InputRef.current.value,
+    ])
+  }
+
+  return <View>
+    <Input type='text' placeholder='请输入待办事项' ref={InputRef}/>
+    <Button onClick={onsubmit}>确认添加</Button>
+    {
+      todoList.map((item, index) => {
+        return <View key={index} className='todo-item'>
+          {item}
+        </View>
+      })
+    }
+  </View>
+}
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -9,7 +9,7 @@ export default function Index () {
 
   return (
     <View className='index'>
-      <Text>Hello world!</Text>
+      <Text>Hello world2!</Text>
     </View>
   )
 }
